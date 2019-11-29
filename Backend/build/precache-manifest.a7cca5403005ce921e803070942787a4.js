57983a8c6a7da643b855ce8d88e603b6d88809bf
MSalah73 <ms24@pdx.edu>
2019-11-30 01:27:39 +0300

    make it better

---
+++ Backend/build/precache-manifest.a7cca5403005ce921e803070942787a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b729e8638ad4ca3d9ca9204ec29401cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f623d92db005913e2216",
+    "url": "/static/css/main.fc228197.chunk.css"
+  },
+  {
+    "revision": "3cac4ca717d213bef66b",
+    "url": "/static/js/2.953e9f19.chunk.js"
+  },
+  {
+    "revision": "f623d92db005913e2216",
+    "url": "/static/js/main.7eebe5a7.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
